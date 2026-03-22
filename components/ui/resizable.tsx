0d634ba0d00d9604a1b86a950f8f9d5f8f979092
shivansh-aajtak <shivansh.aajtak@gmail.com>
2026-03-22 21:02:55 +0530

    QA Fix: Removing all traces of ResizablePrimitive types

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -3,22 +3,15 @@
 import * as React from 'react'
 import { GripVerticalIcon } from 'lucide-react'
 import * as ResizablePrimitive from 'react-resizable-panels'
-
 import { cn } from '@/lib/utils'
 
-// We use ': any' to replace the 'React.ComponentProps' line that is failing
-function ResizablePanelGroup({
-  className,
-  ...props
-}: any) {
+// We have COMPLETELY REMOVED the "ComponentProps" line that Vercel is failing on.
+function ResizablePanelGroup({ className, ...props }: any) {
   const Group: any = (ResizablePrimitive as any)['PanelGroup']
   return (
     <Group
       data-slot="resizable-panel-group"
-      className={cn(
-        'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
-        className,
-      )}
+      className={cn('flex h-full w-full', className)}
       {...props}
     />
   )
@@ -29,19 +22,12 @@ function ResizablePanel({ ...props }: any) {
   return <Panel {...props} />
 }
 
-function ResizableHandle({
-  withHandle,
-  className,
-  ...props
-}: any) {
+function ResizableHandle({ withHandle, className, ...props }: any) {
   const Handle: any = (ResizablePrimitive as any)['PanelResizeHandle']
   return (
     <Handle
       data-slot="resizable-panel-handle"
-      className={cn(
-        'bg-border relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
-        className,
-      )}
+      className={cn('bg-border relative flex w-px items-center justify-center', className)}
       {...props}
     >
       {withHandle && (
